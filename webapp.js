c160e39bdc36f99ca3a02ae410ead30f6cb0f3d7
Jared Forsyth <jared@jaredforsyth.com>
2013-07-30 22:25:08 -0600

    cleanup, docs

--- webapp.js
+++ webapp.js
@@ -13,13 +13,31 @@ function modifyEnv(changefn) {
         status: 'error'
       }, null, '\t'));
     }
-    req.user.get_repo_config(url, function (err, repo, level, user) {
+    req.user.get_repo_config(url, function (err, repo, access_level, user) {
       if (err) return error('Failed to fetch repo config for ' + url + ': ' + err.message)
+      // must have access_level > 0 to be able to continue;
+      if (access_level < 1) {
+        console.debug(
+          "User %s tried to change environment config but doesn't have admin privileges on %s (access level: %s)",
+          req.user.email, url, access_level);
+        return error(
+          "You must have access level greater than 0 in order to be able to configure environment variables.",
+          403)
+      }
       return changefn(req, res, repo, user, error)
     })
   }
 }
 
+/*
+ * DELETE /api/config/env
+ *
+ * Delete a variable from the Strider environment config for specified project.
+ *
+ * @param url Github html_url of the project.
+ * @param key
+ *
+ */
 // to be wrapped by modifyEnv
 function deleteEnv(req, res, repo, user, error) {
   var key = req.param('key')
@@ -36,6 +54,17 @@ function deleteEnv(req, res, repo, user, error) {
   })
 };
 
+
+/*
+ * POST /api/config/env
+ *
+ * Add a variable to the Strider environment config for specified project.
+ *
+ * @param url Github html_url of the project.
+ * @param key
+ * @param val
+ *
+ */
 // to be wrapped by modifyEnv
 function addEnv(req, res, repo, user, error) {
   var key = req.param('key')
@@ -89,74 +118,6 @@ module.exports = function(ctx, cb) {
     })
   }
 
-  /*
-   * POST /api/config/env
-   *
-   * Set the current Strider environment config for specified project.
-   *
-   * @param url Github html_url of the project.
-   * @param env JSON-encoded object representing environment settings.
-   *
-   */
-  function postEnv(req, res) {
-    var url = req.param("url")
-    var env = req.param("env")
-
-    function error(err_msg, code) {
-      console.error("Strider-Env: postEnv() - %s", err_msg)
-      var r = {
-        errors: [err_msg],
-        status: "error"
-      }
-      res.statusCode = 400
-      if (code) res.statusCode = code
-      return res.end(JSON.stringify(r, null, '\t'))
-    }
-
-    req.user.get_repo_config(url, function(err, repo, access_level, owner_user_obj) {
-      if (err) {
-        return error("Error fetching Repo Config for url " + url + ": " + err)
-      }
-      // must have access_level > 0 to be able to continue;
-      if (access_level < 1) {
-        console.debug(
-          "User %s tried to change environment config but doesn't have admin privileges on %s (access level: %s)",
-          req.user.email, url, access_level);
-          return error(
-            "You must have access level greater than 0 in order to be able to configure environment variables.",
-            403)
-      }
-      var invalid = false
-      try {
-        env = JSON.parse(env)
-        if (typeof(env) !== 'object') {
-          invalid = true
-        }
-      } catch(e) {
-        invalid = true
-      }
-      if (invalid) {
-        return error("Error decoding `env` parameter - must be JSON-encoded object")
-      }
-      repo.env = env
-      var r = {
-        status: "ok",
-        errors: [],
-        results: {
-          env:env
-        }
-      }
-      repo.set('env', env)
-      req.user.save(function(err) {
-        if (err) {
-          var errmsg = "Error saving environment config " + req.user.email + ": " + err;
-          return error(errmsg)
-        }
-        return res.end(JSON.stringify(r, null, '\t'))
-      })
-    })
-
-  }
   // Extend RepoConfig model with 'Env' properties
   function envPlugin(schema, opts) {
     schema.add({
@@ -176,7 +137,7 @@ module.exports = function(ctx, cb) {
     ctx.middleware.require_params(['url', 'key', 'val']),
     modifyEnv(addEnv))
   ctx.route.delete('/api/env',
-    ctx.middleware.require_admin,
+    ctx.middleware.require_auth,
     ctx.middleware.require_params(['url', 'key']),
     modifyEnv(deleteEnv))
 
