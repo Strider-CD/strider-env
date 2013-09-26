dae0aa69dc489c3406d71e02ebb2cce87606cc68
Jared Forsyth <jared@jaredforsyth.com>
2013-09-25 23:49:47 -0600

    configuration is now working!

--- config/config.js
+++ config/config.js
@@ -1,16 +1,20 @@
 
 app.controller('EnvironmentCtrl', ['$scope', function ($scope) {
   $scope.$watch('configs[branch].env.config', function (value) {
-    $scope.config = value;
+    $scope.config = value || {};
   });
   $scope.saving = false;
   $scope.save = function () {
     $scope.saving = true;
-    $scope.pluginConfig(name, $scope.config, function () {
+    $scope.pluginConfig('env', $scope.config, function () {
       $scope.saving = false;
     });
   };
   $scope.del = function (key) {
     delete $scope.config[key];
+  };
+  $scope.add = function () {
+    $scope.config[$scope.newkey] = $scope.newvalue;
+    $scope.newkey = $scope.newvalue = '';
   };
 }]);
